4946431e98e16191b3bb18b39b3d73b6e14a6ca2
Üveys Aydemir <33202318+uveysaydemir@users.noreply.github.com>
2025-05-07 13:53:38 +0300

    last version

--- hardhat.config.cjs
+++ hardhat.config.cjs
@@ -3,7 +3,7 @@ require("@nomicfoundation/hardhat-toolbox");
 /** @type import('hardhat/config').HardhatUserConfig */
 module.exports = {
   solidity: {
-    version: "0.8.28",
+    version: "0.8.20",
     settings: {
       optimizer: {
         enabled: true,
@@ -12,8 +12,8 @@ module.exports = {
     },
   },
   networks: {
-    sepolia: {
-      url: "https://sepolia.infura.io/v3/1d171a91871a403ea2511f63cc3445d9",
+    holesky: {
+      url: "https://holesky.infura.io/v3/1d171a91871a403ea2511f63cc3445d9",
       accounts: [
         "37ac36a34c228cac652720a9a009cf9e1b1076e9e9d11de3993ece56aedbb02c",
       ],
